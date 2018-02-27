5361e4ca3c25b89efb890709ea6beea7e49cccc7
Joop Rosier <joop@zodra.nl>
2018-02-27 15:08:41 +0100

    non fatal error in spec discovered, explanation about it added

--- src/app/db-manager/search/search.component.spec.ts
+++ src/app/db-manager/search/search.component.spec.ts
@@ -42,21 +42,23 @@ describe('SearchComponent', () => {
   it('when the user enters values in the searchform, it passes these to the DbManagerService\'s getListItems method', fakeAsync(() => {
     component.listType = 'poems';
     const form = component.searchForm;
-    const stubSearch = {searchFor: 'poems.text', query: '', maxItemsPerPage: 100 };
+    const stubSearch = { searchFor: 'poems.text', query: '', maxItemsPerPage: 100 };
     const spy = spyOn(dbManagerService, 'getListItems');
     form.setValue(stubSearch);
     tick(301);
     expect(spy).toHaveBeenCalledWith(component.listType, stubSearch);
   }));
+  // This test works fine, but it causes an infinite stream of errors that is not visible in the terminal but only in the browser console in a Karma debug window: 
+  // Uncaught Error: macroTask 'setInterval': can not transition to 'running', expecting state 'scheduled', was 'notScheduled'.
 
   it('when the value of the input binding \'listType\' changes (because the user navigated to another list), the form is reset', () => {
     component.listType = 'poems';
     const form = component.searchForm;
-    const stubSearch = {searchFor: 'poems.text', query: 'hi', maxItemsPerPage: '50' };
+    const stubSearch = { searchFor: 'poems.text', query: 'hi', maxItemsPerPage: '50' };
     form.setValue(stubSearch);
     component.listType = 'bundles';
     component.ngOnChanges();
-    const defaultBundlesSearch = {searchFor: 'bundles.title', query: '', maxItemsPerPage: '100'};
+    const defaultBundlesSearch = { searchFor: 'bundles.title', query: '', maxItemsPerPage: '100' };
     expect(form.value).toEqual(defaultBundlesSearch);
   });
 
