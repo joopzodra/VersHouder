9ef8c921deda7de94080a58bce9f6ee66abf7e55
Joop Rosier <joop@zodra.nl>
2018-03-05 12:30:49 +0100

    Added karma firefox launcher and karma safari launcher.

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-safari-launcher'),
+      require('karma-firefox-launcher')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -28,6 +30,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    //browsers: ['Firefox', 'Safari'],
     singleRun: false
   });
 };
